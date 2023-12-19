f1d36df6083bcb8da84ed3e21f4b2510937a6fa7
tonysunny606 <147145222+tonysunny606@users.noreply.github.com>
2023-12-19 15:57:40 +0530

    completed

---
+++ arraysum.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+</head><body>
+	<?php
+	$a=array(52,25,54,54,47,54,87,87,65,23);
+	echo array_sum($a);
+	?>
+</body>
+</html>
---
+++ sum.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+</head>
+<body>
+	<?php
+	$x=4;
+	$y=5;
+	$x=$x+$y;
+	echo $x;
+	?>
+</body>
+</html>
+
